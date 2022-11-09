e2f7c655fd36983d5034e5366a597a39f6709b83
samyaahsan <96796200+samyaahsan@users.noreply.github.com>
2022-11-08 20:23:39 -0500

    alpha-beta pruning w/ expectiminmax implemented:

--- IntelligentAgent.py
+++ IntelligentAgent.py
@@ -7,19 +7,19 @@ class IntelligentAgent(BaseAI):
 
     def getMove(self, grid):
     # Selects a random move and returns it
-        moveset = self.maximize(grid, 0)[0][0]
+        moveset = self.maximize(grid, 0, float('-inf'), float('inf'))[0][0]
         return moveset
 
     def heuristic(self, grid):
-        return grid.getMaxTile()
+        return len(grid.getAvailableCells())
 
     #computer's move
-    def minimize(self, grid, depth):
+    def minimize(self, grid, depth, alpha, beta):
         if self.terminal_test(grid):
             return (None, self.heuristic(grid)) #heuristic == number of empty cells
 
-        if depth >= 3:
-            print("here")
+        if depth >= 4:
+            #print("here")
             return (None, self.heuristic(grid))
 
         (min_child, min_utility) = (None, float('inf'))
@@ -29,32 +29,43 @@ class IntelligentAgent(BaseAI):
             grid2.insertTile(cell, 2)
             grid4 = grid.clone()
             grid4.insertTile(cell, 4)
-            #issue HERE?
-            print("before")
-            utility = (0.9 * self.maximize(grid2, depth + 1)[1]) + (0.1 * self.maximize(grid4, depth + 1)[1])
-            print("after")
+            #print("before")
+            utility = (0.9 * self.maximize(grid2, depth + 1, alpha, beta)[1]) + (0.1 * self.maximize(grid4, depth + 1, alpha, beta)[1])
+            #print("after")
             #(__, utility) = self.maximize(cell, depth + 1)
 
             if utility < min_utility:
                 (min_child, min_utility) = (cell, utility)
 
+            if min_utility <= alpha:
+                break
+
+            if min_utility < beta:
+                beta = min_utility
+
         return (min_child, min_utility)
 
     #player's move
-    def maximize(self, grid, depth):
+    def maximize(self, grid, depth, alpha, beta):
         if not grid.getAvailableMoves():
             return (None, self.heuristic(grid))
 
-        if depth >= 3:
+        if depth >= 4:
             return (None, self.heuristic(grid))
 
         (max_child, max_utility) = (None, float('-inf'))
 
         for cell in grid.getAvailableMoves():
-            utility = self.minimize(grid, depth + 1)[1]
+            utility = self.minimize(grid, depth + 1, alpha, beta)[1]
 
             if utility > max_utility:
                 (max_child, max_utility) = (cell, utility)
+
+            if max_utility >= beta:
+                break
+
+            if max_utility > alpha:
+                alpha = max_utility
 
         #child = (move, grid w/ move)
         return (max_child, max_utility)
